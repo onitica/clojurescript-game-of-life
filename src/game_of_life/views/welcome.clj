3fe164dc6e2029e97ca675d43335314264e069ac
Octavian <onitica@gmail.com>
2012-11-04 13:03:59 -0500

    Added github banner

--- src/game_of_life/views/welcome.clj
+++ src/game_of_life/views/welcome.clj
@@ -12,5 +12,8 @@
 		   [:input {:type "button" :class "button" :id "heatmap-btn" :value "Toggle Heatmap"}]
 	]
 	[:div {:id "itr-div" :height 100 :width 100}]
+	[:a {:href "https://github.com/onitica"}
+		[:img {:style "position: absolute; top: 0; right: 0; border: 0;" :src "https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" :alt "Fork me on GitHub"}]
+	]
   )
 )
